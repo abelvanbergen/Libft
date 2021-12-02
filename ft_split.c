90cbfc464bbb5ee3c6f82bb986bc8de731b4c152
Abel Van bergen en henegouwen <avan-ber@f0r5s11.codam.nl>
2021-12-02 10:49:57 +0100

    verkeerde functie er nog in

--- ft_split.c
+++ ft_split.c
@@ -6,39 +6,12 @@
 /*   By: avan-ber <avan-ber@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/11 09:04:55 by avan-ber      #+#    #+#                 */
-/*   Updated: 2021/10/26 11:45:00 by avan-ber      ########   odam.nl         */
+/*   Updated: 2021/12/02 10:49:21 by avan-ber      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
-
-char	*ft_testsubstr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	size_t	s_len;
-	char	*str;
-
-	i = 0;
-	if (s == 0)
-		return (0);
-	s_len = ft_strlen(s);
-	if (start > s_len)
-		return (ft_strdup(""));
-	if (start + len <= s_len)
-		str = (char *)malloc(len + 1);
-	else
-		str = (char *)malloc(s_len - start + 1);
-	if (str == 0)
-		return (0);
-	while (i < len && s[start + i] != '\0')
-	{
-		str[i] = s[start + i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
 
 static char	**free_array(char **array, int end)
 {
